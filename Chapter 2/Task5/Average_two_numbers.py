4fa53dc9aef9972e212db13e4b955735ffcc7013
Asad Ullah <asadullahdev5@gmail.com>
2026-07-16 01:00:35 +0500

    Complete variables and data types exercises

---
+++ Chapter 2/Task5/Average_two_numbers.py
@@ -0,0 +1,7 @@
+# Average of two numbers entered by the user
+
+num1 = float(input("Enter Your Number Here: "))
+num2 = float(input("Enter Second Number Here: "))
+Average = (num1 + num2) / 2
+
+print("Average: ", Average)
