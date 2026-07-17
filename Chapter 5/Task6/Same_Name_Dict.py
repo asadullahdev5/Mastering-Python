8adf2c313eb8bd73df4253611180c733b4bbaabf
Asad Ullah <asadullahdev5@gmail.com>
2026-07-17 11:30:58 +0500

    Completed Dictionary and Sets

---
+++ Chapter 5/Task6/Same_Name_Dict.py
@@ -0,0 +1,10 @@
+# if name of two friends are same what will to the program
+
+Dict = {}
+
+Dict.update({"Friend1" : "Python"})
+Dict.update({"Friend2" : "Java"})
+Dict.update({"Friend3" : "C++"})
+Dict.update({"Friend4" : "Python"})
+
+print(Dict)
---
+++ Chapter 5/Task7/Change_List_Values.py
@@ -0,0 +1,7 @@
+# Can we change a values inside a list Conatin in SET s
+
+S = {8 ,7,12,"Asad",[1,2]}
+print(S)
+
+#  No you cannot. A List cannot be inside a set becuase lists 
+# are mutable and unhashable. So this set S is inavlid
