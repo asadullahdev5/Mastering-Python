5453597df052ac72d160818b171f5968235cfebe
Asad Ullah <asadullahdev5@gmail.com>
2026-07-24 11:48:36 +0500

    Chapter 8 Functions & Recurssions

---
+++ Chapter 8/Task8/Func_Multiplication.py
@@ -0,0 +1,6 @@
+# Write a program to print multiplication table using Function
+
+def Multiplication(table):
+    for i in range (1, 10+1):
+        print (table ,"x" , i , "=", table * i)
+Table = Multiplication(10)
