afe41181b6980389e726033d0f7ce281c67e20e0
Asad Ullah <asadullahdev5@gmail.com>
2026-07-24 11:04:46 +0500

    Chapter 8 Functions & Recurssions

---
+++ Chapter 8/Task4/Create_Function.py
@@ -0,0 +1,13 @@
+# create function first n line of following pattren 
+# *
+# **
+# ***
+# ****
+
+
+
+def Create_Function(Pattren):
+    for i in range(1, Pattren+1):
+        print("*" * i)
+
+a = Create_Function(5)
---
+++ Chapter 8/Task5/Func_Inches_To_Cm.py
@@ -0,0 +1,22 @@
+# Inches to cm 
+
+def cm_to_inches(Centimeter):
+    inches = Centimeter/2.54
+    return inches
+
+CM = 165
+inch = cm_to_inches(CM)
+
+print(f"{CM} Centimeter ={inch:.2f} Inches")
+
+
+# Cm to Feet
+
+def cm_to_inches(Centimeter):
+    feet = Centimeter/30.48
+    return feet
+
+CM = 170
+Feet = cm_to_inches(CM)
+
+print(f"{CM} Centimeter ={Feet:.2f} Feets")
